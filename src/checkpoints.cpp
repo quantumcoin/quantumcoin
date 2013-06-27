6fcc5ca081840ed8543bba1335750bc6837f0402
Christian Berendt <sauerkraut42@gmail.com>
2013-06-27 07:37:50 +0200

    added checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -36,6 +36,10 @@ namespace Checkpoints
         (  9000, uint256("0x52774f08d627266d1bf91042940aee29dc2d7b843e7b40caf456378e23d2c13f"))
         ( 10000, uint256("0xabcac989dfa666c36416b9c8afbbc27c6a12fdf8d881da20802c27604731571b"))
         ( 11000, uint256("0xa2f2744b8a45c6542ce9e754ca8125b74a6a00526df4ba24421bd74eaadf51e3"))
+        ( 12000, uint256("0x5b46ac0cab098a83af23a716412586ecf175f15e48ace8f422f5779ed81335fe"))
+        ( 13000, uint256("0xbc0375c0b316fd6b827f6c70a20f5270d99c64b718725b1fe8ef0562fcde3be5"))
+        ( 14000, uint256("0x32ae15bdafc6006bab99c84cec9e3770f5b8a93f4999ef048b1824e608a29f45"))
+        ( 15000, uint256("0xe4e7943015dec90cccb5e104de6cd7ff215e76b67756ffc446cb23c852bb9390"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
